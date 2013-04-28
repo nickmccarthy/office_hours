30a299980e04bbda8ba8cc0399d8c38f7f6ba88b
nick <nmccarthy@outlook.com>
2013-04-28 18:23:03 -0400

    Adding php comments, and change name of search location in form

--- dashboard.php
+++ dashboard.php
@@ -28,3 +28,24 @@ if(isset($_SESSION['user'])) {
 
 </body>
 </html>
+
+
+
+<?php
+
+// TODO:
+
+// query users with uid in session to get name
+
+// query users join teaches join class with their uid
+// for each, print a line with
+// Department . Number . (Position)
+
+// For each of these, check permissions this user has
+// use level from previous query and see what permissions that level has
+// in class join permissions
+
+// print office hours link for each and also, if have permission to change
+// course permissions, print a link for that too
+
+?>
