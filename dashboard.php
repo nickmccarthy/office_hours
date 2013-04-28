c633457ae2c2cd48f270aeeb202c80aab328faae
nick <nmccarthy@outlook.com>
2013-04-28 17:03:50 -0400

    php comments

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,16 @@
 <?
+// query users with uid in session to get name
+
+// query users join teaches join class with their uid
+// for each, print a line with
+// Department . Number . (Position)
+
+// For each of these, check permissions this user has
+// use level from previous query and see what permissions that level has
+// in class join permissions
+
+// print office hours link for each and also, if have permission to change
+// course permissions, print a link for that too
+
 print 'dashboard';
 ?>
