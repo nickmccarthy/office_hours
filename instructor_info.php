0634381925b16fa06ea1cfbc235902ff7ff040e0
nick <nmccarthy@outlook.com>
2013-04-28 21:08:20 -0400

    more comments

--- instructor_info.php
+++ instructor_info.php
@@ -29,7 +29,8 @@ if(isset($_GET['uid'])){
 // TODO:
 
 // Query users join teaches join class for the supplied uid (can only get here
-// if the query had a unique match or the user chose a result, meaning this is // known)
+// if the query had a unique match or the user chose a result, meaning this is 
+// known)
 
 // Print the name of this person as well as each of their classes and associated
 // office hours (officeHours table)
