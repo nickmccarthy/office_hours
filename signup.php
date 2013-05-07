066b70251080d3c709e38cbf0b28dcca487c5209
nick <nmccarthy@outlook.com>
2013-05-07 16:04:57 -0400

    Add example of automatically changing css each month

--- signup.php
+++ signup.php
@@ -1,60 +1,62 @@
 <?php
-	session_start();
+session_start();
 
 	// Import db config info
-	require 'config/mysql.config.php';
-	require 'config/pageinfo.config.php';
+require 'config/mysql.config.php';
+require 'config/pageinfo.config.php';
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="styles/styles.css">
-    <!-- style sheets will change depending on the month -->
+	<!-- style sheets will change depending on the month 
+	<link rel="stylesheet" type="text/css" href="styles/<?php echo strtolower(date('F'))?>.css"> -->
 	<link rel="stylesheet" type="text/css" href="styles/april.css">
+
 	<link href='http://fonts.googleapis.com/css?family=Acme' rel='stylesheet' type='text/css' />
 	<link href='http://fonts.googleapis.com/css?family=Gudea' rel='stylesheet' type='text/css' />
 </head>
 <body id="tab2">
-<?php
-if(isset($_SESSION['user'])) {
-	require 'inc/header_in.html';
-} else {
-	require 'inc/header.html';
-}
-?>
-<div class="content">
-    <div class="center">
-	<ul id="tabnav">
-	    <li class="tab1"><a href="login.php">Login</a></li>
-	    <li class="tab2"><a href="signup.php">Sign Up</a></li>
-	</ul>
-	<div class="tabarea">
-	    <form method="post" action="login.php">
-                <div class="line">
-		    <label for="fname">First Name</label>
-                    <input type="text" name="fname" id="fname" placeholder="First Name"/>
+	<?php
+	if(isset($_SESSION['user'])) {
+		require 'inc/header_in.html';
+	} else {
+		require 'inc/header.html';
+	}
+	?>
+	<div class="content">
+		<div class="center">
+			<ul id="tabnav">
+				<li class="tab1"><a href="login.php">Login</a></li>
+				<li class="tab2"><a href="signup.php">Sign Up</a></li>
+			</ul>
+			<div class="tabarea">
+				<form method="post" action="login.php">
+					<div class="line">
+						<label for="fname">First Name</label>
+						<input type="text" name="fname" id="fname" placeholder="First Name"/>
+					</div>
+					<div class="line">
+						<label for="lname">Last Name</label>
+						<input type="text" name="lname" id="lname" placeholder="Last Name"/>
+					</div>
+					<div class="line">
+						<label for="username">Email</label>
+						<input type="text" name="username" id="username" placeholder="netID@cornell.edu"/>
+					</div>
+					<div class="line">
+						<label for="password">Password</label>
+						<input type="password" name="password" id="password" placeholder="password"/>
+					</div>
+					<div class="line">
+						<label for="terms"></label>
+						<button type="submit">Sign Up</button>
+					</div>
+				</form>
+			</div>
 		</div>
-		<div class="line">
-                    <label for="lname">Last Name</label>
-		    <input type="text" name="lname" id="lname" placeholder="Last Name"/>
-		</div>
-		<div class="line">
-		    <label for="username">Email</label>
-		    <input type="text" name="username" id="username" placeholder="netID@cornell.edu"/>
-		</div>
-		<div class="line">
-		    <label for="password">Password</label>
-		    <input type="password" name="password" id="password" placeholder="password"/>
-		</div>
-		<div class="line">
-		    <label for="terms"></label>
-		    <button type="submit">Sign Up</button>
-		</div>
-	    </form>
 	</div>
-    </div>
-</div>
 </body>
 </html>
 
