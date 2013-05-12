bba2b9cd43e40e5c03e412d0641e4759aadf9d57
tingtingwu <tw283@cornell.edu>
2013-05-11 23:12:47 -0400

    edit permissions styling

--- edit_permissions.php
+++ edit_permissions.php
@@ -11,7 +11,7 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="styles/styles.css">
     <!-- style sheets will change depending on the month -->
-	<!--<link rel="stylesheet" type="text/css" href="styles/april1.css">-->
+	<link rel="stylesheet" type="text/css" href="styles/<?php echo strtolower(date('F'))?>.css">
 	<link href='http://fonts.googleapis.com/css?family=Acme' rel='stylesheet' type='text/css' />
 	<link href='http://fonts.googleapis.com/css?family=Gudea' rel='stylesheet' type='text/css' />
 </head>
@@ -25,28 +25,29 @@
     }
 ?>
 <div class="content">
-    <h2>Course ID | Settings</h2>
-    <p><a href="dashboard.php">Back to Dashboard</a></p>
+    <h2>Course ID | Course Permissions</h2>
+    <div class="center">
     <form action="edit_permissions.php?cid=<?php print($_GET['cid']); ?>" method="post">
-        Toggle Inactive: <input type="checkbox" name="inactive" value="1"><br />
-        Change Course Name: <input type="text" name="coursename"><br />
-        Drop any of the following from being an instructor:<br />
-<?php
-    require 'config/mysql.config.php';
-    require 'config/pageinfo.config.php';
-    $mydb = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
-    if (mysqli_connect_errno()) {
-        printf("Connect failed: %s\n", mysqli_connect_error());
-        exit();
-    }
-    $query = "SELECT first_name, last_name, uid FROM Teaches NATURAL JOIN Users WHERE cid=" . $_GET['cid'];
-    $teachers = $mydb->query($query);
-    while($row = $teachers->fetch_assoc()){
-        print("<label for " . $row['uid'] . "><input type='checkbox' name='teachers[]' value='" . $row['uid'] . "'>" . $row['first_name'] . " " . $row['last_name'] . "<br />");
-    }
-?> 
-        <input type="submit" value="Submit">
-    </form>
+        <div class="left"><span class="edit_form">Toggle Inactive </span><span class="right"><input type="checkbox" name="inactive" value="1"></span></div>
+        <div class="left"><span class="edit_form">Change Course Name </span><span class="right"><input type="textarea" name="coursename" placeholder="New Course Name"></div></span>
+        <div class="left"><span class="edit_form">Drop any of the following from being an instructor:</span></div>
+        <?php
+            require 'config/mysql.config.php';
+            require 'config/pageinfo.config.php';
+            $mydb = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
+            if (mysqli_connect_errno()) {
+                printf("Connect failed: %s\n", mysqli_connect_error());
+                exit();
+            }
+            $query = "SELECT first_name, last_name, uid FROM Teaches NATURAL JOIN Users WHERE cid=" . $_GET['cid'];
+            $teachers = $mydb->query($query);
+            while($row = $teachers->fetch_assoc()){
+                print("<div class=\"left\"><span class=\"edit\"><label for=" . $row['uid'] . "><input type='checkbox' name='teachers[]' value='" . $row['uid'] . "'> " . $row['first_name'] . " " . $row['last_name'] . "</label></span></div>");
+            }
+        ?> 
+        <div class="lineoh"><button type="submit" class="oh_btn">Submit</button></div>
+        </form>
+    </div>
 </div>
 
 </body>
